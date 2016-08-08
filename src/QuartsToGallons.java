f326669dbcbe196906070b279ba2121c33c21641
kyecook <acfanatics@hotmail.com>
2016-08-08 10:10:36 +1000

    Initial commit

---
+++ src/QuartsToGallons.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+/**
+ * Created by kyecook on 8/08/2016.
+ */
+public class QuartsToGallons {
+    public static void main(String[] args)
+    {
+        int QUARTS_IN_A_GALLON = 4;
+        int paintingJob = 18;
+        int calculatedPaintingJobQuarts;
+        int calculatedPaintingJobGallons;
+
+        calculatedPaintingJobQuarts = paintingJob%QUARTS_IN_A_GALLON;
+        calculatedPaintingJobGallons = paintingJob/QUARTS_IN_A_GALLON;
+
+        System.out.println("A job that needs " + paintingJob + " quarts requires " + calculatedPaintingJobGallons
+                + " Gallons plus " + calculatedPaintingJobQuarts + " Quarts.");
+
+    }
+}
